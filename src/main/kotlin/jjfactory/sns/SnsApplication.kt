92d7790b9175d0e3b5e75c95d353ea5c708e08f5
jaehyeong <wogud1514@gmail.com>
2023-07-01 23:06:30 +0900

    init

---
+++ src/main/kotlin/jjfactory/sns/SnsApplication.kt
@@ -0,0 +1,11 @@
+package jjfactory.sns
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SnsApplication
+
+fun main(args: Array<String>) {
+	runApplication<SnsApplication>(*args)
+}
---
+++ src/test/kotlin/jjfactory/sns/SnsApplicationTests.kt
@@ -0,0 +1,13 @@
+package jjfactory.sns
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SnsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
